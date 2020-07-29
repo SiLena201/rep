e933c7ba975b28090da38c425238b65bb81c8ded
SiLena201 <61623671+SiLena201@users.noreply.github.com>
2020-07-29 15:43:13 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,225 @@
+/*document.addEventListener("DOMContentLoaded", function(event) { 
+ const modal = document.querySelector('.modal');
+ const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+ const closeBtn = document.querySelector('.modal__close');
+ const switchModal = () => {
+  modal.classList.toggle('modal--visible');
+ }
+ 
+ modalBtn.forEach(element => {
+  element.addEventListener('click', switchModal);
+ });
+
+  closeBtn.addEventListener('click', switchModal);
+ 
+});
+*/
+
+var progress = document.querySelector('.progress'); 
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e) {
+  var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight; 
+  var per = windowScroll / windowHeight * 100;
+
+  progress.style.width = per + '%';
+}
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+      subscribe = $('.subscribe');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  }); 
+
+  $(document).ready(function(){
+    $('.modal--visible').hide();
+    $('.modal').click(function(){
+        $('.modal--visible').show();
+    });
+    $(this).keydown(function(eventObject){
+        if (eventObject.which == 27)
+            $('.modal--visible').hide();
+    });
+  });
+
+  $(document).mouseup(function (e) {
+    var modal = $('.modal');
+    if (e.target!=modal[0]&&modal.has(e.target).length === 0) {
+      $('.modal--visible').hide();
+    }
+    
+  });
+
+  $(function() {
+ 
+    $(window).scroll(function() {
+     
+    if($(this).scrollTop() != 0) {
+     
+    $('#toTop').fadeIn();
+     
+    } else {
+     
+    $('#toTop').fadeOut();
+     
+    }
+     
+    });
+     
+
+  
+    $('#toTop').click(function() {
+     
+    $('body,html').animate({scrollTop:0},1500);
+     
+    });
+     
+    });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    allowTouchMove: false,
+    pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+  
+  next.css('left', next.width() +20 +bullets.width() +20);
+  bullets.css('left', prev.width() +20);
+
+  new WOW().init();
+
+  $(".modal__form").validate({
+   errorClass:  "invalid",
+    errorElement: "div",
+   rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: {
+        required: true,
+        phone: true
+      },
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче 2 букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: {
+        required: "Заполните поле",
+      },
+      userEmail: {
+        required: "Заполните поле",
+        email: "Введите корректный email"
+      }
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          alert('Форма отправлена.');
+          console.log('AJAX сработал. Форма отправлена' + response);
+        }
+      });
+    }
+  });
+
+  // маска для номера телефона
+  $('[type=tel]').mask('+7 (000) 000-00-00', { placeholder: "Ваш номер телефона" });
+
+  $(".control__form").validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: {
+        required: true,
+        phone: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче 2 букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: {
+        required: "Заполните поле",
+      }
+    }
+  });
+
+  $(".footer__form").validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      // Строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: {
+        required: true,
+        phone: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче 2 букв",
+        maxlength: "Имя не больше 15 букв"
+      },
+      userPhone: {
+        required: "Заполните поле",
+      }
+    }
+  });
+  
+
+});
+
+// $(document).ready(function() {
+//   $('ul.section__tabs').on('click', 'li:not(.section__tab_active)', function() {
+//     $(this)
+//       .addClass('section__tab_active').siblings().removeClass('section__tab_active')
+//       .closest('div.container').find('div.section__content').removeClass('section__content_active').eq($(this).index()).addClass('section__content_active');
+  
+//     });
+//   });  
